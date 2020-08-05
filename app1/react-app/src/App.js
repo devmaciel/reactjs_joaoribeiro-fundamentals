c7ce170707c943deeb9a0a293981aaaf8bc47860
João Pedro Maciel <joomaciiel2@outlook.com>
2020-08-05 19:35:05 -0300

    aula_35

--- app1/react-app/src/App.js
+++ app1/react-app/src/App.js
@@ -1,13 +1,14 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div>
-      Olá mundo
-    </div>
-  );
+class App extends React.Component{
+	render(){
+		return(
+			<div>
+				Olá mundo
+			</div>
+		)
+	}
 }
+
 
 export default App;
