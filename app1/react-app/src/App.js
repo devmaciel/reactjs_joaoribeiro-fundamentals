63a8fef90ac08f4b7335537325062e5881528b02
João Pedro Maciel <joomaciiel2@outlook.com>
2020-08-06 17:51:18 -0300

    aula_41

--- app1/react-app/src/App.js
+++ app1/react-app/src/App.js
@@ -1,24 +1,12 @@
 import React from 'react';
-import Child from './Child';
+import './App.css';
 
 class App extends React.Component{
-
-	state = {
-		nome: "Default",
-	}
-
-	Alterar = (novoNome) =>{
-		this.setState({nome: novoNome});
-	}
 
 	render(){
 		return(
 			<div>
-				<h3>Parent</h3>
-				<button onClick={this.Alterar('Pedro')}>Alterar</button>
-				<hr/>
-				<Child nome={this.state.nome}/>
-				<Child funcaoAlterar = {this.Alterar}/>
+				<h3 className="cor-texto">Texto da minha aplicação</h3>
 			</div>
 		)
 	}
@@ -26,3 +14,31 @@ class App extends React.Component{
 
 
 export default App;
+// import React from 'react';
+// import Child from './Child';
+
+// class App extends React.Component{
+
+// 	state = {
+// 		nome: "Default",
+// 	}
+
+// 	Alterar = (novoNome) =>{
+// 		this.setState({nome: novoNome});
+// 	}
+
+// 	render(){
+// 		return(
+// 			<div>
+// 				<h3>Parent</h3>
+// 				<button onClick={this.Alterar('Pedro')}>Alterar</button>
+// 				<hr/>
+// 				<Child nome={this.state.nome}/>
+// 				<Child funcaoAlterar = {this.Alterar}/>
+// 			</div>
+// 		)
+// 	}
+// }
+
+
+// export default App;
--- app1/react-app/src/Child.js
+++ app1/react-app/src/Child.js
@@ -1,20 +1,20 @@
-import React, { Component } from 'react';
+// import React, { Component } from 'react';
 
-class Child extends Component {
+// class Child extends Component {
 
-    AlterarChild = () =>{
-        this.props.funcaoAlterar('João');
-    }
+//     AlterarChild = () =>{
+//         this.props.funcaoAlterar('João');
+//     }
 
-    render() {
-        return (
-            <div>
-                <h3>Child</h3>
-                <p>Nome: {this.props.nome}</p>
-                <button onClick={this.AlterarChild}>Alterar</button>
-            </div>
-        );
-    }
-}
+//     render() {
+//         return (
+//             <div>
+//                 <h3>Child</h3>
+//                 <p>Nome: {this.props.nome}</p>
+//                 <button onClick={this.AlterarChild}>Alterar</button>
+//             </div>
+//         );
+//     }
+// }
 
-export default Child;
+// export default Child;
